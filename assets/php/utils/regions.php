022c7a977fc51e326930d383fb0bdb4deb460ad0
Sharkov Ivan <sharkov.agent@mail.ru>
2023-01-16 21:55:25 +0300

    Create correct array sort of region
    -- Fix sort array of regions

--- assets/php/utils/regions.php
+++ assets/php/utils/regions.php
@@ -6,7 +6,7 @@ $regions = array();
 // -- Create variable for regions
 $query = mysqli_query($connect, "SELECT * FROM number_of_victims");
 while ($row = mysqli_fetch_assoc($query)) {
-    array_push($regions, $row["subject"]);
+    array_push($regions, trim($row["subject"]));
 }
 
 $regions = array_unique($regions);
