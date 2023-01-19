3b5787c3cb95db2e0a3708146e8e4e6185c5c05b
Sharkov Ivan <sharkov.agent@mail.ru>
2023-01-19 11:21:37 +0300

    Articles text review
    -- Review articles text in articles.php

--- assets/php/components/literature.php
+++ assets/php/components/literature.php
@@ -6,15 +6,23 @@
                 <ul class="literature__list">
                     <li class="literature__list-item">1. МИНИСТЕРСТВО ВНУТРЕННИХ ДЕЛ
                         ПО РЕСПУБЛИКЕ АЛТАЙ. Как защитить себя от уличной преступности. [Электронный ресурс]. URL:
-                        https://04.xn--b1aew.xn--p1ai/document/827650 (дата обращения: 14.01.2023). </li>
+                        https://04.xn--b1aew.xn--p1ai/document/827650 (дата обращения: 14.01.2023).
+                    </li>
                     <li class="literature__list-item">2. Елена Тимошкина. Pravda.ru. Караул, грабят! Можно ли
                         уберечься? [Электронный ресурс]. URL:
-                        https://www.pravda.ru/society/1278450-grishin/ (дата обращения: 14.01.2023).</li>
+                        https://www.pravda.ru/society/1278450-grishin/ (дата обращения: 14.01.2023).
+                    </li>
                     <li class="literature__list-item">3. Дмитрий Сергеевич Томко. Amulex.daily. Что делать, если
                         вымогают
                         деньги и угрожают. [Электронный ресурс]. URL:
                         https://amulex.ru/daily/chto-delat-esli-vymogayut-dengi-i-ugrozhayut/ (дата обращения:
-                        14.01.2023).</li>
+                        14.01.2023).
+                    </li>
+                    <li class="literature__list-item">4. Группа компаний "ТАГГЕРД". Меры противодействия угрозам,
+                        вымогательству, шантажу. [Электронный ресурс]. URL:
+                        https://www.taggerd.su/info/mery-protivodeystviya-vymogatelstvu-i-shantazhu/ (дата обращения:
+                        14.01.2023).
+                    </li>
                 </ul>
             </div>
         </div>
